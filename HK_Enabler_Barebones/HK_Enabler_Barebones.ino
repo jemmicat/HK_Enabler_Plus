d153cc84d613773c7b734807fd135068e026d5eb
jemmicat <jem7784@hotmail.com>
2013-11-26 10:49:52 -0600

    Reorganized Folders and added files

--- HK_Enabler_Barebones/HK_Enabler_Barebones.ino
+++ HK_Enabler_Barebones/HK_Enabler_Barebones.ino
@@ -2,9 +2,7 @@
 // Uses Sparkfun Arduino Pro Micro (Or equivalent)https://www.sparkfun.com/products/11098
 // Uses TH3122.4 chip Sourced from http://www.ebay.com/itm/2x-Melexis-TH3122-4-IBUS-I-Bus-K-Bus-Transceiver-/221296386461?pt=LH_DefaultDomain_0&hash=item33864ae19d
 // Uses PC Board Sourced From OSH Park (3 boards for $5 sq in)
-// connections for 10DOF(axis) IMU L3G4200D+ADXL345+HMC5883L+BMP085 (3V-5V compatible)sensor module http://www.ebay.com/itm/141096879704?ssPageName=STRK:MEWNX:IT&_trksid=p3984.m1497.l2649
-// IMU Also here https://core-electronics.com.au/store/index.php/10dof-imu-module-with-l3g4200d-adxl345-hmc5883l-bmp085-gy-80.html 
-// Holes For attching https://www.sparkfun.com/products/9394
+
 // Send Patterns from the I-Bus/K-Bus for Harman Kardon Radio Emulation 
 // based on https://github.com/blalor/iPod_IBus_adapter
 // Idea From http://www.northamericanmotoring.com/forums/electrical/155161-getting-more-out-of-the-r53-mfsw.html
@@ -22,17 +20,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <SoftwareSerial.h>
-#include <Wire.h>
 
-///////////////////////////////////////////////////////////////////////////
 // Pin/Button connections...
 //PCB has connections for 4 buttons at D2, D4, D5, and D6
-#define DSP_mode    14
-#define SENSTA      4 //if SEN/STA is jumpered to Ground, D4 is open  
-#define Volume_plus 15
-#define Fader_front 16
-#define Fader_rear  10
+#define DSP_mode    2
+#define SENSTA      3
+#define Volume_plus 4
+#define Fader_front 5
+#define Fader_rear  6
 
 /*
  * LEDs:
@@ -42,17 +37,9 @@
  *   
  *   yellow + green: contention/collision when sending
  */
-#define LED_ERR      5 // red
-#define LED_IBUS_RX  6 // yellow
-#define LED_IBUS_TX  7 // green
-
-//i2C connections for IMU
-#define i2C_SDA 2 
-#define i2C_SCL 3
-
-SoftwareSerial mySerial(9, 8); // RX, TX for LCD screen
-
-/////////////////////////////////////////////////////////////
+#define LED_ERR      10 // red
+#define LED_IBUS_RX  16 // yellow
+#define LED_IBUS_TX  14 // green
 
 #define MAX_EXPECTED_LEN 64
 
@@ -74,25 +61,6 @@ boolean Radio_turned_on;
 boolean Radio_ready;
 int packet_delay = 30;
 
-//ADXL345 Defines
-#define Register_ID 0
-#define Register_2D 0x2D
-#define Register_X0 0x32
-#define Register_X1 0x33
-#define Register_Y0 0x34
-#define Register_Y1 0x35
-#define Register_Z0 0x36
-#define Register_Z1 0x37
-
-int ADXAddress = 0xA7 >> 1;  // the default 7-bit slave address
-int reading = 0;
-int val=0;
-int X0,X1,X_out;
-int Y0,Y1,Y_out;
-int Z1,Z0,Z_out;
-double Xg,Yg,Zg;
-
-
 // buffer for building outgoing packets
 uint8_t tx_buf[TX_BUF_LEN];
 
@@ -111,7 +79,7 @@ unsigned long readTimeout; // variable
 
 
 void setup() { 
-  pinMode (SENSTA, INPUT);
+  pinMode (SENSTA, OUTPUT);
   pinMode (DSP_mode,INPUT_PULLUP);
   pinMode (Fader_front, INPUT_PULLUP);
   pinMode (Fader_rear, INPUT_PULLUP);
@@ -119,9 +87,7 @@ void setup() {
   
   Radio_turned_on = false; 
   Radio_ready = false;
-  
-  Wire.begin();
-  
+          
   // set up serial for IBus; 9600,8,E,1 I set these up directly as they didn't seem to work right using Arduino call out
     Serial1.begin(9600);
     UCSR1C |= (1 << UPM11);
@@ -129,16 +95,7 @@ void setup() {
     UCSR1C |= (0 << USBS1);
     
   //start recieving Serial Data 
-    Serial.begin(9600);
-   
-  //LCD Serial
-   mySerial.begin(9600); 
-   
-  // enable to measute g data
-  Wire.beginTransmission(ADXAddress);
-  Wire.write(Register_2D);
-  Wire.write(8);                //measuring enable
-  Wire.endTransmission();     // stop transmitting
+    Serial.begin(9600); 
    
 } 
 
