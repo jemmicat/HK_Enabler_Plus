3e5d035e294d71c92cf3db15cbcfb3b4c2a8d7b5
jemmicat <jem7784@hotmail.com>
2013-12-29 18:13:01 -0600

    added more delay before sending codes

--- HK_Enabler_Barebones/HK_Enabler_Barebones.ino
+++ HK_Enabler_Barebones/HK_Enabler_Barebones.ino
@@ -212,6 +212,7 @@ void decode_packet(const uint8_t *packet) {
 // {{{ send_radio settings
 void Send_radio_settings() {
       
+    delay(200);
     Send_initialize_begin();
     delay(packet_delay); 
     Send_bass_level();
